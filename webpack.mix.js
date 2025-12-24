c5d5b9cec4408d66458f13533f5be5e007fd4706
zacknathan87 <zacknathan87@users.noreply.github.com>
2025-12-24 18:18:30 +0000

    Update npm dependencies - fix security vulnerabilities
    - Downgraded pusher-js from ^8.4.0-rc2 to ^7.6.0 (fixes cluster error)
    - Updated all dependencies to latest secure versions
    - Reduced vulnerabilities from 87 to 10
    - Added vue-loader for Vue 2.x support
    - Updated scripts to use mix CLI

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,6 +13,7 @@ const mix = require("laravel-mix");
 
 mix.js("resources/js/app.js", "public/js")
 .js("resources/js/admin.js", "public/js")
-    .sass("resources/sass/app.scss", "public/css")
-    .sass("resources/sass/main.scss", "public/css")
-    .version();
+     .vue()
+     .sass("resources/sass/app.scss", "public/css")
+     .sass("resources/sass/main.scss", "public/css")
+     .version();
