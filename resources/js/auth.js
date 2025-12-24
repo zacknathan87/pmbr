0a2f11bd64be77d82b09eabec94d3cbe21e43490
zacknathan87 <zacknathan87@users.noreply.github.com>
2025-12-24 18:31:42 +0000

    Fix vue-auth driver - use axios.1.x driver for compatibility

--- resources/js/auth.js
+++ resources/js/auth.js
@@ -1,5 +1,5 @@
 import bearer from '@websanova/vue-auth/drivers/auth/bearer'
-import axios from '@websanova/vue-auth/drivers/http/frisbee.1.x'
+import axios from '@websanova/vue-auth/drivers/http/axios.1.x'
 import router from '@websanova/vue-auth/drivers/router/vue-router.2.x'
 
 // Auth base configuration some of this options
